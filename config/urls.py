f2516fae281e1fb2b9218cf7e3b231f25fe865c4
Jihyun-Choi <034179@naver.com>
2024-08-22 02:17:34 +0900

    feat: swagger 설정

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,49 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Feed Flow API",
+      default_version='v1',
+      description="feed-flow-backend",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="WantedPreOnboardingDjango@local.dev"),
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(
+        "",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        'admin/',
+        admin.site.urls
+    ),
+    re_path(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    re_path(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    re_path(
+        r"^redoc/$",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
