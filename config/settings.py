9e80279fda05515493f732e2358ec5d77a605fc8
dollyishere <rundollyrun8@gmail.com>
2024-08-23 02:07:03 +0900

    feat: user, article app 생성

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "drf_yasg",
+    "user",
+    "company",
 ]
 
 MIDDLEWARE = [
--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
