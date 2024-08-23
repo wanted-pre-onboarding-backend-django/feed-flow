37c61355aa54f55cba65629669c09ddf157473ea
dollyishere <rundollyrun8@gmail.com>
2024-08-24 00:26:08 +0900

    feat : 게시글 좋아요 api 구현

---
+++ article/views/article_like_views.py
@@ -0,0 +1,54 @@
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import get_object_or_404
+
+from models import Article
+
+
+class ArticleLikeAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def patch(self, request, *args, **kwargs):
+        """
+        PATCH : /articles/:id/like/
+        게시물 좋아요 버튼 클릭 시, like_cnt 1씩 증가
+
+        TODO: 나중에 에러 메시지 한 파일에 모아서 관리할 것
+        TODO: 해당하는 사이트의 좋아요 기능과 연결하기
+        """
+        try:
+            # path param으로 article_id 추출
+            article_id = kwargs.get("id")
+
+            # 게시글 id가 전달되지 않을 시, 오류
+            if not article_id:
+                raise ValidationError("게시글 번호가 필요합니다.")
+
+            # 해당하는 article 객체 조회
+            # 없을 시, 404 Error 발생
+            article = get_object_or_404(Article, id=article_id)
+
+            # like_cnt 증가
+            article.like_cnt += 1
+            article.save()
+
+            # 성공 시, 204 응답
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        # 에러 처리
+        except ValidationError as e:
+            return Response({"msg": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Article.DoesNotExist:
+            return Response(
+                {"msg": "해당하는 게시글이 없습니다."}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        except Exception as e:
+            return Response(
+                {"msg": f"{str(e)} 오류가 발생했습니다. 다시 시도해주세요."},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
