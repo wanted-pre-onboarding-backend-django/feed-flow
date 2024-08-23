557769942d0305c6fe4588e0ef022859697cd565
dollyishere <rundollyrun8@gmail.com>
2024-08-24 00:26:08 +0900

    feat : 게시글 좋아요 api 구현

--- article/urls.py
+++ article/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from .views import ArticleLikeAPIView
+
 app_name = "article"
 router = DefaultRouter()
 
-urlpatterns = []
+urlpatterns = [
+    path(
+        "articles/<int:article_id>/like/",
+        ArticleLikeAPIView.as_view(),
+        name="article-like",
+    )
+]
 
 urlpatterns += router.urls
---
+++ article/views/__init__.py
@@ -0,0 +1,1 @@
+from article_like_views import ArticleLikeAPIView
