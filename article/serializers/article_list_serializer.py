e2b8e9fca9e52740bd624394630e35c3baf20c03
Hess <hess90wang@gmail.com>
2024-08-25 03:39:26 +0900

    feat: 게시물리스트 부가구현

--- article/serializers/article_list_serializer.py
+++ article/serializers/article_list_serializer.py
@@ -1,16 +1,27 @@
 from rest_framework.serializers import ModelSerializer
-from article.models import Article
+from rest_framework import serializers
+from article.models import Article, Hashtag
+
+
+class HashtagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hashtag
+        fields = ["name"]
 
 
 class ArticleListSerializer(ModelSerializer):
-    """게시물 리스트 시리얼라이저"""
+
+    hashtag = HashtagSerializer(many=True, read_only=True)
 
     class Meta:
         model = Article
-        fields = (
-            "pk",
-            "title",
-            "type",
-            "content_id",
-            "like_cnt",
+        fields = "__all__"
+
+    # 필드 커스텀을 위한 함수 to_representation함수(장고함수)
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        # content 필드를 20자로 제한하여 반환
+        representation["content"] = instance.content[:20] + (
+            "..." if len(instance.content) > 20 else ""
         )
+        return representation
--- article/views/article_list_view.py
+++ article/views/article_list_view.py
@@ -13,26 +13,62 @@ from article.serializers import ArticleDetailSerializer, ArticleListSerializer
 class ArticlesView(APIView):
     """게시글 리스트 뷰"""
 
+    serializer_class = ArticleListSerializer
+
     def get(self, request):
-        # 페이징
-        try:
-            page = request.query_params.get("page", 1)
-            # 페이지를 쿼리로 가져온다
-            page = int(page)
-        except ValueError:
-            # 없을시 기본으로 1 페이지로
-            page = 1
-        page_size = 5
-        # 각 페이지 내 게시글갯수
-        start = (page - 1) * page_size
-        # 페이지 시작 pk
-        end = start + page_size
-        # 페이지 마지막 게시물 pk
+        filtered = Article.objects.all()
+        print(filtered)
+        query_params = request.query_params
+
+        if "hashtag" in query_params:
+            filtered = filtered.filter(hashtag__name__exact=query_params["hashtag"])
+        else:
+            if request.user.is_authenticated:
+                filtered = filtered.filter(hashtag__name__exact=request.user.account)
+        if "type" in query_params:
+            filtered = filtered.filter(type__exact=query_params["type"])
+
+        search_by = query_params.get("search_by", "title,content")
+        if "search" in query_params:
+            if search_by == "title":
+                # 제목에서만 검색
+                filtered = filtered.filter(title__icontains=query_params["search"])
+            elif search_by == "content":
+                # 내용에서만 검색
+                filtered = filtered.filter(content__icontains=query_params["search"])
+            else:
+                # 제목과 내용에서 검색
+                search_result = filtered.filter(title__icontains=query_params["search"])
+                cotent_result = filtered.filter(
+                    content__icontains=query_params["search"]
+                )
+                filtered = search_result.union(cotent_result)
+        order_filed = query_params.get("order_by", "created_at")
+        valid_order_by = [
+            "created_at",
+            "updated_at",
+            "like_cnt",
+            "share_cnt",
+            "view_cnt",
+            "-created_at",
+            "-updated_at",
+            "-like_cnt",
+            "-share_cnt",
+            "-view_cnt",
+        ]
+        if order_filed in valid_order_by:
+            filtered = filtered.order_by(order_filed)
+
+        page = int(query_params.get("page", 1))
+        page_count = int(query_params.get("page_count", 3))
+        start = (page - 1) * page_count
+        end = start + page_count
+
         serializer = ArticleListSerializer(
-            Article.objects.all()[start:end],
-            # 해당 페이지 게시물들가져오기
+            filtered[start:end],
             many=True,
         )
+
         return Response(serializer.data)
 
     def post(self, request):
