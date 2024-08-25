d21d61a56e2fbf83768434f8eed0e92b11874266
Hess <hess90wang@gmail.com>
2024-08-25 13:48:03 +0900

    fix: 불필요 임포트 삭제

--- article/views/article_detail_view.py
+++ article/views/article_detail_view.py
@@ -1,11 +1,9 @@
 from rest_framework.views import APIView
-from django.db import transaction
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
 from rest_framework.exceptions import (
     NotFound,
     NotAuthenticated,
-    ParseError,
     PermissionDenied,
 )
 from article.models import Article, Hashtag
--- article/views/article_list_view.py
+++ article/views/article_list_view.py
@@ -1,10 +1,8 @@
 from rest_framework.views import APIView
-from django.db import transaction
 from rest_framework.status import HTTP_204_NO_CONTENT
 from rest_framework.response import Response
 from rest_framework.exceptions import (
     NotAuthenticated,
-    ParseError,
 )
 from article.models import Hashtag, Article
 from article.serializers import ArticleDetailSerializer, ArticleListSerializer
